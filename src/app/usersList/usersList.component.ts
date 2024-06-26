dd6d68c084f54f1788eeac4cba2b7a21aa3c7dd7
José Esteves de Souza Neto <email@jose-desouza.com>
2024-06-26 16:53:29 +0200

    adding new user to the list

--- src/app/usersList/usersList.component.ts
+++ src/app/usersList/usersList.component.ts
@@ -7,32 +7,80 @@ import { Component } from "@angular/core";
 })
 export class UsersListComponent {
     // Put Business Logic on the components and not on the templates
-    loggedUser =  {
-        id: 1,
-        name: 'Kermit the Frog',
-        age: 21
-    }
+    selectedUser = {
+        id: '',
+        name: '',
+        age: ''
+    };
 
     allUsers = [
         {
-            id: 1,
+            id: this.generateId(),
             name: 'Kermit the Frog',
-            age: 21
+            age: '21'
         },
         {
-            id: 2,
+            id: this.generateId(),
             name: 'Piggy the Pig',
-            age: 19
+            age: '19'
         },
         {
-            id: 3,
+            id: this.generateId(),
             name: 'Elmo The Animal',
-            age: 19
+            age: '19'
         },
         {
-            id: 4,
+            id: this.generateId(),
             name: 'Baby Sitter',
-            age: 30
+            age: '30'
         }
     ]
+
+    loggedUser = this.allUsers[0];
+
+    setName(event: Event): void {
+        let name: string = (event.target as HTMLInputElement).value;
+        this.selectedUser.name = name;
+        console.log('setName', name);
+    }
+
+    setAge(event: Event): void {
+        let age: string = (event.target as HTMLInputElement).value;
+        this.selectedUser.age = age;
+        console.log('setAge', age);
+    }
+
+    generateId(): string {
+        return Math.random().toString(16);
+    }
+
+    resetSelectedUser(): void {
+        this.selectedUser = {
+            id: '',
+            name: '',
+            age: ''
+        };
+    }
+
+    save(): void {
+        if (this.selectedUser.name === '' || this.selectedUser.age === '') {
+            return;
+        }
+
+        if (this.selectedUser.id === '') {
+            this.selectedUser.id = this.generateId();
+        }
+
+        this.allUsers.push(this.selectedUser);
+
+        console.log('save', this.selectedUser.id, this.selectedUser.name, this.selectedUser.age);
+        this.resetSelectedUser();
+    }
+
+    remove(id: string): void {
+        if (this.loggedUser.id === id) {
+            return;
+        }
+        this.allUsers = this.allUsers.filter(user => user.id !== id);
+    }
 }
